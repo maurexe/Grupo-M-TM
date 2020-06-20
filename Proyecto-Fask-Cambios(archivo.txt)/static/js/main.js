f4ee1841a52677b0dc6fd3fee99639f47a78f39a
AlexisFredes <63241281+AlexisFredes@users.noreply.github.com>
2020-06-19 23:57:24 -0300

    Update main.js

--- Proyecto-Fask-Cambios(archivo.txt)/static/js/main.js
+++ Proyecto-Fask-Cambios(archivo.txt)/static/js/main.js
@@ -1,1 +1,13 @@
-
+function initMap(){
+  const ubicacion = new Localizacion(()=>{
+    const options = {
+      center: {
+        lat: ubicacion.latitude,
+        lng: ubicacion.longitude
+      },
+      zoom: 15
+    }
+    var map = document.getElementById('map');
+    const mapa =new google.maps.Map(map, options)
+  });
+}
