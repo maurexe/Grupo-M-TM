46f76b0408a58fc8649e58ba751c5a07df1dfa57
AlexisFredes <63241281+AlexisFredes@users.noreply.github.com>
2020-06-21 22:43:21 -0300

    actualizando

--- Proyecto/static/js/main.js
+++ Proyecto/static/js/main.js
@@ -1,1 +1,17 @@
+function initMap(){
+  const ubicacion = new Localizacion(()=>{
+    const myLatLng= {lat: ubicacion.latitude, lng: ubicacion.longitude};
+    const options = {
+      center: myLatLng,
+      zoom: 15
+    };
+    var map = document.getElementById('map');
+    const mapa =new google.maps.Map(map, options);
+    const marcador = new google.maps.Marker({
+      position: myLatLng,
+      map: mapa,
+      title:"AppConductor"
+    });
+  });
+}
 
